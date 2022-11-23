2beff46a0eef5dcd21be0015ea015467b3ccab03
gcjurgiel <gcjurgiel@icloud.com>
2022-11-23 11:15:42 -0600

    Rewrite of base motor controller interfaces

--- include/rmb/motorcontrol/PositionController.h
+++ include/rmb/motorcontrol/PositionController.h
@@ -1,97 +1,290 @@
 #pragma once
 
-#include <units/base.h>
+#include <units/length.h>
 #include <units/velocity.h>
+#include <units/acceleration.h>
+#include <units/angle.h>
+#include <units/angular_velocity.h>
+#include <units/angular_acceleration.h>
 
 namespace rmb {
 
 /**
- * Base class for wrappers around vendor API / device specific
- * positionController code. This class provides a standard interface to code
- * that allows you to control the position of a motor.
- * @tparam User specified distance unit
+ * Interface for controlling motor position used by wrappers of device specific 
+ * APIs. The interface is meant to descibe what a motor should do, but not how 
+ * should do it. Methods are given in both linear and angular units but controll 
+ * the same motor, so changes to the target linear velocity also modify the 
+ * target angular velocity and vice versa.
+ * 
+ * @see SparkMaxPositionController TalonSRXPositionController 
+ *      FalconPositionController
  */
-template <typename DistanceUnit> class PositionController {
+class PositionController {
 public:
-  using Distance_t = units::unit_t<DistanceUnit>; /**< User specified distance
-                                                     unit. Is probably going to
-                                                     be an angle or a length*/
-  using VelocityUnit =
-      units::compound_unit<DistanceUnit,
-                           units::inverse<units::seconds>>; /**< DistanceUnit
-                                                               per second*/
-  using Velocity_t = units::unit_t<
-      VelocityUnit>; /**< Type definition for
-                        PositionController<DistanceUnit>::VelocityUnit*/
-  using AccelerationUnit = units::compound_unit<
-      VelocityUnit,
-      units::inverse<units::seconds>>; /**< DistanceUnit per second squared */
-  using Acceleration_t = units::unit_t<
-      AccelerationUnit>; /**< Type definition for
-                            PositionController<DistanceUnit>::AccelerationUnit*/
 
   /**
-   * Resets the reference point of the position controller to base position
-   * readings off of.
-   * @param position the desired reference point of the position controller in
-   * user specified DistanceUnits
+   * Common interface for setting the target linear position.
+   * 
+   * @param position The target linear position in meters.
    */
-  virtual void resetReference(Distance_t position) = 0;
+  virtual void setPosition(units::meter_t position) = 0;
 
   /**
-   * Set the maximum position that the motor controller can go to.
-   * @param max the maximum position in user specified DistanceUnits
+   * Common interface for setting the target angular position.
+   * 
+   * @param position The target angular position in radians.
    */
-  virtual void setMaxPosition(Distance_t max) = 0;
+  virtual void setAngularPosition(units::radian_t position) = 0;
 
   /**
-   * Get the maximum position of the motorcontroller
-   * @return the maximum position in user specified DistanceUnits
+   * Common interface for setting the target linear position as offset from the 
+   * previouse target.
+   * 
+   * @param position The offset from the previouse target in meters.
    */
-  virtual Distance_t getMaxPosition() = 0;
+  virtual void setPositionOffset(units::meter_t position) = 0;
 
   /**
-   * Set the minimum position the motorcontroller can go to.
-   * @param min the minimum position in user specified DistanceUnits
+   * Common interface for setting the target angular position as offset from the 
+   * previouse target.
+   * 
+   * @param position The offset from the previouse target in radians.
    */
-  virtual void setMinPosition(Distance_t min) = 0;
+  virtual void setAngularPositionOffset(units::radian_t position) = 0;
 
   /**
-   * Get the minimum position the motorcontroller can go to
-   * @return the minimum position in user specified DistanceUnits
+   * Common interface for getting the <b>current</b> linear position.
+   * 
+   * @return The <b>current</b> linear position in meters
    */
-  virtual Distance_t getMinPosition() = 0;
+  virtual units::meter_t getPosition() const = 0;
 
   /**
-   * Sets the desired position of the motor.
-   * @param position the desired position in user specified DistanceUnits
+   * Common interface for getting the <b>current</b> angular position.
+   * 
+   * @return the <b>current</b> angular position in radians.
    */
-  virtual void setPosition(Distance_t position) = 0;
+  virtual units::radian_t getAngularPosition() const = 0;
 
   /**
-   * Get the current position of the motor.
-   * @return the current position of the motor in user specified DistanceUnits
+   * Common interface for getting the <b>target</b> linear position.
+   * 
+   * @return The <b>target</b> linear position in meters.
    */
-  virtual Distance_t getPosition() const = 0;
+  virtual units::meter_t getTargetPosition() const = 0;
 
   /**
-   * Get the current velocity of the motor
-   * @return the current velocity of the motor in user specified DistanceUnits
+   * Common interface for getting the <b>target</b> angular position.
+   * 
+   * @return The <b>target</b> angular position in meters.
    */
-  virtual Velocity_t getVelocity() = 0;
+  virtual units::radian_t getTargetAngularPosition() const = 0;
 
   /**
-   * Invert the motor (make it go reverse from the default)
-   * @param inverted whether or not the motor should be inverted. true if
-   * inverted.
+   * Common interface for getting the <b>current</b> linear position error.
+   * 
+   * @return The diffrence between the actual linear position and target linear 
+   *         position in meters.
    */
-  virtual void setInverted(bool inverted) = 0;
+  virtual units::meter_t getError() = 0;
 
   /**
-   * Get whether or not the motor is inverted
-   * @return true if the motor is inverted
+   * Common interface for getting the <b>current</b> angular velocity error.
+   * 
+   * @return The diffrence between the actual angular velocity and target 
+   *         angular velocity in radians.
    */
-  virtual bool getInverted() = 0;
+  virtual units::radian_t getAngularError() = 0;
+
+  /**
+   * Common interface for returning whetther the motor is at the target position.
+   * 
+   * @return true if it at the target, and false if it is not.
+   */
+  virtual bool atTarget() = 0;
+
+  /**
+   * Common interface for getting the current linear velocity.
+   * 
+   * @return The current linear velocity in meters per second.
+   */
+  virtual units::meters_per_second_t getVelocity() = 0;
+
+  /**
+   * Common interface for getting the current angular velocity.
+   * 
+   * @return The current angular velocity in radians per second.
+   */
+  virtual units::radians_per_second_t getAngularVelocity() = 0;
+
+  /**
+   * Common interface for zeroing the linear position such that the 
+   * <b>target</b> linear position is remapped to the given position. 
+   * 
+   * @param position The values the <b>target</b> linear position will be 
+   *                 remapped to.
+   */
+  virtual void zeroPosition(units::meter_t position = 0_m) = 0;
+
+  /**
+   * Common interface for zeroing the angular position such that the 
+   * <b>target</b> angular position is remapped to the given position. 
+   * 
+   * @param position The values the <b>target</b> angular position will be
+   *                 remapped to.
+   */
+  virtual void zeroAngularPosition(units::radian_t position = 0_rad) = 0;
+
+  /**
+   * Common interface for setting the maximum linear position.
+   * 
+   * @param max The maximum linear position in meters.
+   */
+  virtual void setMaxPosition(units::meter_t max) = 0;
+
+  /**
+   * Common interface for setting the maximum angular position.
+   * 
+   * @param max The maximum angular position in radians.
+   */
+  virtual void setMaxAngularPosition(units::radian_t max) = 0;
+
+  /**
+   * Common interface for getting the maximum linear position.
+   * 
+   * @return The maximum linear position in meters.
+   */
+  virtual units::meter_t getMaxPosition() = 0;
+
+  /**
+   * Common interface for getting the maximum angular position.
+   * 
+   * @return The maximum angular position in radians.
+   */
+  virtual units::radian_t getMaxAngularPosition() = 0;
+
+  /**
+   * Common interface for setting the minimum linear position.
+   * 
+   * @param min The minimum linear position in meters.
+   */
+  virtual void setMinPosition(units::meter_t min) = 0;
+
+  /**
+   * Common interface for setting the minimum angular position.
+   * 
+   * @param min The minimum angular position in radians.
+   */
+  virtual void setMinAngularPosition(units::radian_t min) = 0;
+
+  /**
+   * Common interface for getting the minimum linear position.
+   * 
+   * @return The minimum linear position in meters.
+   */
+  virtual units::meter_t getMinPosition() = 0;
+
+    /**
+   * Common interface for sgetting the minimum angular position.
+   * 
+   * @return The minimum angular position in radians.
+   */
+  virtual units::radian_t getMinAngularPosition() = 0;
+
+  /**
+   * Common interface for setting the maximum linear velocity.
+   * 
+   * @param max The maximum linear velocity in meters per second.
+   */
+  virtual void setMaxVelocity(units::meters_per_second_t max) = 0;
+
+  /**
+   * Common interface for setting the maximum angular velocity.
+   * 
+   * @param max The maximum angular velocity in radians per second.
+   */
+  virtual void setMaxAngularVelocity(units::radians_per_second_t max) = 0;
+
+  /**
+   * Common interface for getting the maximum linear velocity.
+   * 
+   * @return The maximum linear velocity in meters per second.
+   */
+  virtual units::meters_per_second_t getMaxVelocity() = 0;
+
+  /**
+   * Common interface for getting the maximum angular velocity.
+   * 
+   * @return The maximum angular velocity in radians per second.
+   */
+  virtual units::radians_per_second_t getMaxAngularVelocity() = 0;
+
+  /**
+   * Common interface for setting the maximum linear acceleration.
+   * 
+   * @param max The maximum linear acceleration in meters per second squared.
+   */
+  virtual void setMaxAcceleration(units::meters_per_second_squared_t max) = 0;
+
+  /**
+   * Common interface for setting the maximum angular acceleration.
+   * 
+   * @param max The maximum angular acceleration in radians per second squared.
+   */
+  virtual void setMaxAngularAcceleration(units::radians_per_second_squared_t max) = 0;
+
+  /**
+   * Common interface for getting the maximum linear acceleration.
+   * 
+   * @return The maximum linear acceleration in meters per second squared.
+   */
+  virtual units::meters_per_second_squared_t getMaxAcceleration() = 0;
+
+  /**
+   * Common interface for getting the maximum angular acceleration.
+   * 
+   * @return The maximum angular acceleration in radians per second squared.
+   */
+  virtual units::radians_per_second_squared_t getMaxAngularAcceleration() = 0;
+
+  /**
+   * Common interface for setting the coversion factor from linear to angular units.
+   * 
+   * @param conversionFactor The conversion factor from linear to angular units (such as a wheel
+   *                         diameter or gear ratio)
+   */
+  virtual void setConversion(units::compound_unit<units::radians, units::inverse<units::meters>> conversionFactor) = 0;
+
+  /**
+   * Common interface for getting the coversion factor from linear to angular units.
+   * 
+   * @return The conversion factor from linear to angular units (such as a wheel diameter or gear ratio)
+   */
+  virtual units::compound_unit<units::radians, units::inverse<units::meters>> getConversion() = 0;
+
+    /**
+   * Common interface for inverting direction of a motor.
+   *
+   * @param isInverted The state of inversion, true is inverted.
+   */
+  virtual void setInverted(bool isInverted) = 0;
+
+    /**
+   * Common interface for returning the inversion state of a motor.
+   *
+   * @return isInverted The state of inversion, true is inverted.
+   */
+  virtual bool getInverted() const = 0;
+
+  /**
+   * Common interface for disabling a motor.
+   */
+  virtual void disable() = 0;
+
+  /**
+   * Common interface to stop the motor until `SetPosition` is called again.
+   */
+  virtual void stopMotor() = 0;
 };
 
 } // namespace rmb
--- include/rmb/motorcontrol/VelocityController.h
+++ include/rmb/motorcontrol/VelocityController.h
@@ -1,58 +1,187 @@
+
 #pragma once
 
-#include <units/base.h>
-#include <units/time.h>
+#include <units/velocity.h>
+#include <units/acceleration.h>
+#include <units/angular_velocity.h>
+#include <units/angular_acceleration.h>
+
+#include <frc/motorcontrol/MotorController.h>
 
 namespace rmb {
 
 /**
- * The base class for wrappers around classes that abstract over device-specific
- * apis to control the velocity at which a motor spins.
+ * Interface for controlling motor velocity used by wrappers of device specific 
+ * APIs. The interface is meant to descibe what a motor should do, but not how 
+ * should do it. Methods are given in both linear and angular units but controll 
+ * the same motor, so changes to the target linear velocity also modify the 
+ * target angular velocity and vice versa.
+ * 
+ * @see SparkMaxVelocityController TalonSRXVelocityController 
+ *      FalconVelocityController
  */
-template <typename DistanceUnit> class VelocityController {
+class VelocityController {
 public:
-  using Distance_t =
-      units::unit_t<DistanceUnit>; /**< User specified distance units*/
-  using VelocityUnit = units::compound_unit<
-      DistanceUnit, units::inverse<units::seconds>>; /**< User specified
-                                                        distance unit / second*/
-  using Velocity_t = units::unit_t<
-      VelocityUnit>; /**< Type definition for
-                        VelocityController<DistanceUnit>::VelocityUnit*/
-  using AccelerationUnit =
-      units::compound_unit<VelocityUnit,
-                           units::inverse<units::seconds>>; /**< User specified
-                                                               distance unit /
-                                                               second^2*/
-  using Acceleration_t = units::unit_t<
-      AccelerationUnit>; /**< Type definition of
-                            VelocityController<DistanceUnit>::AccelerationUnit*/
 
   /**
-   * Sets the target velocity of the motor.
-   * @param velocity the target velocity in user specified velocity units
-   * @see Velocity_t
+   * Common interface for setting the target linear velocity.
+   * positon
+   * 
+   * @param velocity The target linear velocity in meters per second.
    */
-  virtual void setVelocity(Velocity_t velocity) = 0;
+  virtual void setVelocity(units::meters_per_second_t velocity) = 0;
 
   /**
-   * Get the <b>current</b> velocity of the motor
-   * @return the velocity of the motor in user specified velocity units
-   * @see Velocity_t
+   * Common interface for setting the target angular velocity.
+   * 
+   * @param velocity The target angular velocity in radians per second.
    */
-  virtual Velocity_t getVelocity() = 0;
+  virtual void setAngularVelocity(units::radians_per_second_t velocity) = 0;
 
   /**
-   * Toggles motor inversion
-   * @param inverted whether or not the motor should be inverted. true if
-   * inverted.
+   * Common interface for getting the <b>current</b> linear velocity of the
+   * motor regardless of target.
+   * 
+   * @return The <b>current</b> linear velocity in meters per second.
    */
-  virtual void setInverted(bool inverted) = 0;
+  virtual units::meters_per_second_t getVelocity() = 0;
 
   /**
-   * Get whether or not the motor is inverted
-   * @return whether or not the motor is inverted. true if inverted.
+   * Common interface for getting the <b>current</b> angular velocity of the
+   * motor regardless of target.
+   * 
+   * @return The <b>current</b> angular velocity in radians per second
    */
-  virtual bool getInverted() = 0;
+  virtual units::radians_per_second_t getAngularVelocity() = 0;
+
+  /**
+   * Common interface for getting the <b>target</b> linear velocity.
+   * 
+   * @return The <b>target</b> linear velocity in meters per second.
+   */
+  virtual units::meters_per_second_t getTargetVelocity() = 0;
+
+  /**
+   * Common interface for getting the <b>target</b> angular velocity.
+   * 
+   * @return The <b>target</b> angular velocity in radians per second.
+   */
+  virtual units::radians_per_second_t getTargetAngularVelocity() = 0;
+
+  /**
+   * Common interface for getting the <b>current</b> linear velocity error.
+   * 
+   * @return The diffrence between the actual linear velocity and target linear velocity.
+   */
+  virtual units::meters_per_second_t getError() = 0;
+
+  /**
+   * Common interface for getting the <b>current</b> angular velocity error.
+   * 
+   * @return The diffrence between the actual angular velocity and target angular velocity.
+   */
+  virtual units::radians_per_second_t getAngularError() = 0;
+
+  /**
+   * Common interface for returning whetther the motor is at the target velocity.
+   * 
+   * @return true if it at the target, and false if it is not.
+   */
+  virtual bool atTarget() = 0;
+
+  /**
+   * Common interface for setting the maximum desired linear velocity.
+   * 
+   * @param maxVelocity The maximum linear velocity of a motor.
+   */
+  virtual void setMaxVelocity(units::radians_per_second_t maxVelocity) = 0;
+
+  /**
+   * Common interface for setting the maximum desired angular velocity.
+   * 
+   * @param maxVelocity The maximum angular velocity of a motor.
+   */
+  virtual void setMaxAngularVelocity(units::radians_per_second_t maxVelocity) = 0;
+
+  /**
+   * Common interface for getting the maximum desired linear velocity.
+   * 
+   * @return The maximum linear velocity of a motor.
+   */
+  virtual units::radians_per_second_t getMaxVelocity() = 0;
+
+  /**
+   * Common interface for getting the maximum desired angular velocity.
+   * 
+   * @return The maximum angular velocity of a motor.
+   */
+  virtual units::radians_per_second_t getMaxAngularVelocity() = 0;
+
+  /**
+   * Common interface for setting the maximum desired linear accleration.
+   * 
+   * @param maxAcceleration The maximum linear acceleration of a motor.
+   */
+  virtual void setMaxAcceleration(units::meters_per_second_squared_t maxAcceleration) = 0;
+
+  /**
+   * Common interface for setting the maximum desired angular accleration.
+   * 
+   * @param maxAcceleration The maximum angular acceleration of a motor.
+   */
+  virtual void setMaxAngularAcceleration(units::radians_per_second_squared_t maxAcceleration) = 0;
+
+  /**
+   * Common interface for getting the maximum desired linear accleration.
+   * 
+   * @return The maximum linear acceleration of a motor.
+   */
+  virtual units::meters_per_second_squared_t getMaxAcceleration() = 0;
+
+  /**
+   * Common interface for getting the maximum desired angular accleration.
+   * 
+   * @return The maximum angular acceleration of a motor.
+   */
+  virtual units::radians_per_second_squared_t getMaxAngularAcceleration() = 0;
+
+  /**
+   * Common interface for setting the coversion factor from linear to angular units.
+   * 
+   * @param conversionFactor The conversion factor from linear to angular units (such as a wheel
+   *                         diameter or gear ratio)
+   */
+  virtual void setConversion(units::compound_unit<units::radians, units::inverse<units::meters>> conversionFactor) = 0;
+
+  /**
+   * Common interface for getting the coversion factor from linear to angular units.
+   * 
+   * @return The conversion factor from linear to angular units (such as a wheel diameter or gear ratio)
+   */
+  virtual units::compound_unit<units::radians, units::inverse<units::meters>> getConversion() = 0;
+
+  /**
+   * Common interface for inverting direction of a motor.
+   *
+   * @param isInverted The state of inversion, true is inverted.
+   */
+  virtual void setInverted(bool isInverted) = 0;
+
+  /**
+   * Common interface for returning the inversion state of a motor.
+   *
+   * @return isInverted The state of inversion, true is inverted.
+   */
+  virtual bool getInverted() const = 0;
+
+  /**
+   * Common interface for disabling a motor.
+   */
+  virtual void disable() = 0;
+
+  /**
+   * Common interface to stop the motor until `setVelocity` is called again.
+   */
+  virtual void stopMotor() = 0;
 };
 } // namespace rmb
