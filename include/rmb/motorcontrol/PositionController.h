e718f55ad72520b796b8f82b144e05a6a0133b74
gcjurgiel <gcjurgiel@icloud.com>
2022-12-03 16:20:41 -0600

    Added support for non-linear conversions

--- include/rmb/motorcontrol/PositionController.h
+++ include/rmb/motorcontrol/PositionController.h
@@ -10,54 +10,58 @@
 namespace rmb {
 
 /**
- * Interface for controlling motor position used by wrappers of device specific 
- * APIs. The interface is meant to descibe what a motor should do, but not how 
- * should do it. Methods are given in both linear and angular units but controll 
- * the same motor, so changes to the target linear velocity also modify the 
- * target angular velocity and vice versa.
+ * Interface for controlling a mechanism's position used by wrappers of device 
+ * specific APIs. The interface is meant to descibe what a mechanism should do, 
+ * not how it should do it. Methods are given in both linear and angular 
+ * units but controll the same mechanism, so changes to the target linear 
+ * position also modify the target angular position and vice versa. Linear 
+ * methods are given default implintations that rely on angular the methods 
+ * under the assumtion that the mechanism is powered by a motor and that the 
+ * conversion from angular to linear units id defined for all angles. If this 
+ * is not the case, re-implement or mark methods as unimplemented as nessesary.
  * 
  * @see SparkMaxPositionController TalonSRXPositionController 
- *      FalconPositionController
+ *      FalconPositionController Servo
  */
 class PositionController {
 public:
 
   /**
-   * Common interface for setting the target linear position.
+   * Common interface for setting the target linear position. 
    * 
    * @param position The target linear position in meters.
    */
-  virtual void setPosition(units::meter_t position) = 0;
+  virtual void setPosition(units::meter_t position);
 
   /**
-   * Common interface for setting the target angular position.
+   * Common interface for setting the target angular position. 
    * 
    * @param position The target angular position in radians.
    */
   virtual void setAngularPosition(units::radian_t position) = 0;
 
   /**
-   * Common interface for setting the target linear position as offset from the 
-   * previouse target.
+   * Common interface for setting the target linear position as an offset 
+   * from the current target. 
    * 
    * @param position The offset from the previouse target in meters.
    */
-  virtual void setPositionOffset(units::meter_t position) = 0;
+  virtual void setPositionOffset(units::meter_t position);
 
   /**
-   * Common interface for setting the target angular position as offset from the 
-   * previouse target.
+   * Common interface for setting the target angular position as offset from 
+   * the previouse target.
    * 
    * @param position The offset from the previouse target in radians.
    */
-  virtual void setAngularPositionOffset(units::radian_t position) = 0;
+  virtual void setAngularPositionOffset(units::radian_t position);
 
   /**
    * Common interface for getting the <b>current</b> linear position.
    * 
    * @return The <b>current</b> linear position in meters
    */
-  virtual units::meter_t getPosition() const = 0;
+  virtual units::meter_t getPosition() const;
 
   /**
    * Common interface for getting the <b>current</b> angular position.
@@ -71,7 +75,7 @@ public:
    * 
    * @return The <b>target</b> linear position in meters.
    */
-  virtual units::meter_t getTargetPosition() const = 0;
+  virtual units::meter_t getTargetPosition() const;
 
   /**
    * Common interface for getting the <b>target</b> angular position.
@@ -86,203 +90,155 @@ public:
    * @return The diffrence between the actual linear position and target linear 
    *         position in meters.
    */
-  virtual units::meter_t getError() = 0;
+  virtual units::meter_t getError() const;
 
   /**
-   * Common interface for getting the <b>current</b> angular velocity error.
+   * Common interface for getting the <b>current</b> angular velocity error. 
    * 
    * @return The diffrence between the actual angular velocity and target 
    *         angular velocity in radians.
    */
-  virtual units::radian_t getAngularError() = 0;
+  virtual units::radian_t getAngularError() const;
 
   /**
-   * Common interface for returning whetther the motor is at the target position.
+   * Common interface for returning whether the mechanism is at the target 
+   * position.
    * 
    * @return true if it at the target, and false if it is not.
    */
-  virtual bool atTarget() = 0;
-
-  /**
-   * Common interface for getting the current linear velocity.
-   * 
-   * @return The current linear velocity in meters per second.
-   */
-  virtual units::meters_per_second_t getVelocity() = 0;
-
-  /**
-   * Common interface for getting the current angular velocity.
-   * 
-   * @return The current angular velocity in radians per second.
-   */
-  virtual units::radians_per_second_t getAngularVelocity() = 0;
+  virtual bool atTarget() const = 0;
 
   /**
    * Common interface for zeroing the linear position such that the 
-   * <b>target</b> linear position is remapped to the given position. 
+   * <b>target</b> linear position is remapped to the provided position. If the
+   * provided offset would put the mechanism out of bounds, the next clossest 
+   * offset used and the function returns false.
+   * 
+   * <p><b>Note/b> this function will also remapp min and max.</p>
    * 
    * @param position The values the <b>target</b> linear position will be 
    *                 remapped to.
    */
-  virtual void zeroPosition(units::meter_t position = 0_m) = 0;
+  virtual bool zeroPosition(units::meter_t offset = 0_m);
 
   /**
    * Common interface for zeroing the angular position such that the 
-   * <b>target</b> angular position is remapped to the given position. 
+   * <b>target</b> angular position is remapped to the given position. If the
+   * provided offset would put the mechanism out of bounds, the next clossest 
+   * offset used and the function returns false. 
+   * 
+   * <p><b>Note</b> this function will also remapp min and max values.</p> 
    * 
    * @param position The values the <b>target</b> angular position will be
    *                 remapped to.
    */
-  virtual void zeroAngularPosition(units::radian_t position = 0_rad) = 0;
+  virtual bool zeroAngularPosition(units::radian_t offset = 0_rad) = 0;
 
   /**
-   * Common interface for setting the maximum linear position.
+   * Common interface for setting the maximum linear position. If the given
+   * maximum is invalid the closest valid maximum is used and the function 
+   * returns false.
    * 
    * @param max The maximum linear position in meters.
    */
-  virtual void setMaxPosition(units::meter_t max) = 0;
+  virtual bool setMaxPosition(units::meter_t max);
 
   /**
-   * Common interface for setting the maximum angular position.
+   * Common interface for setting the maximum angular position. If the given
+   * maximum is invalid the closest valid maximum is used and the function 
+   * returns false.
    * 
    * @param max The maximum angular position in radians.
    */
-  virtual void setMaxAngularPosition(units::radian_t max) = 0;
+  virtual bool setMaxAngularPosition(units::radian_t max) = 0;
 
   /**
    * Common interface for getting the maximum linear position.
    * 
    * @return The maximum linear position in meters.
    */
-  virtual units::meter_t getMaxPosition() = 0;
+  virtual units::meter_t getMaxPosition() const;
 
   /**
    * Common interface for getting the maximum angular position.
    * 
    * @return The maximum angular position in radians.
    */
-  virtual units::radian_t getMaxAngularPosition() = 0;
+  virtual units::radian_t getMaxAngularPosition() const = 0;
 
   /**
-   * Common interface for setting the minimum linear position.
+   * Common interface for setting the minimum linear position. If the given
+   * minimum is invalid the closest valid minimum is used and the function 
+   * returns false.
    * 
    * @param min The minimum linear position in meters.
    */
-  virtual void setMinPosition(units::meter_t min) = 0;
+  virtual bool setMinPosition(units::meter_t min);
 
   /**
-   * Common interface for setting the minimum angular position.
+   * Common interface for setting the minimum angular position. If the given
+   * minimum is invalid the closest valid minimum is used and the function 
+   * returns false.
    * 
    * @param min The minimum angular position in radians.
    */
-  virtual void setMinAngularPosition(units::radian_t min) = 0;
+  virtual bool setMinAngularPosition(units::radian_t min) = 0;
 
   /**
    * Common interface for getting the minimum linear position.
    * 
    * @return The minimum linear position in meters.
    */
-  virtual units::meter_t getMinPosition() = 0;
+  virtual units::meter_t getMinPosition() const;
 
     /**
    * Common interface for sgetting the minimum angular position.
    * 
    * @return The minimum angular position in radians.
    */
-  virtual units::radian_t getMinAngularPosition() = 0;
+  virtual units::radian_t getMinAngularPosition() const = 0;
 
   /**
-   * Common interface for setting the maximum linear velocity.
+   * Common interface for converting from linear to angular units. A functions
+   * is used rather than a conversion factor since some conversions may be
+   * non-linear. 
    * 
-   * @param max The maximum linear velocity in meters per second.
+   * @param linear The linear distance in meters.
+   * @return The angular distance in radians.
    */
-  virtual void setMaxVelocity(units::meters_per_second_t max) = 0;
+  virtual units::radian_t linearToAngular(units::meter_t linear) const = 0;
 
   /**
-   * Common interface for setting the maximum angular velocity.
+   * Common interface for converting from angular to linear units. A functions
+   * is used rather than a conversion factor since some conversions may be
+   * non-linear. 
    * 
-   * @param max The maximum angular velocity in radians per second.
+   * @param angle The linear distance in radians.
+   * @return The linear distance in meters.
    */
-  virtual void setMaxAngularVelocity(units::radians_per_second_t max) = 0;
-
-  /**
-   * Common interface for getting the maximum linear velocity.
-   * 
-   * @return The maximum linear velocity in meters per second.
-   */
-  virtual units::meters_per_second_t getMaxVelocity() = 0;
-
-  /**
-   * Common interface for getting the maximum angular velocity.
-   * 
-   * @return The maximum angular velocity in radians per second.
-   */
-  virtual units::radians_per_second_t getMaxAngularVelocity() = 0;
-
-  /**
-   * Common interface for setting the maximum linear acceleration.
-   * 
-   * @param max The maximum linear acceleration in meters per second squared.
-   */
-  virtual void setMaxAcceleration(units::meters_per_second_squared_t max) = 0;
-
-  /**
-   * Common interface for setting the maximum angular acceleration.
-   * 
-   * @param max The maximum angular acceleration in radians per second squared.
-   */
-  virtual void setMaxAngularAcceleration(units::radians_per_second_squared_t max) = 0;
-
-  /**
-   * Common interface for getting the maximum linear acceleration.
-   * 
-   * @return The maximum linear acceleration in meters per second squared.
-   */
-  virtual units::meters_per_second_squared_t getMaxAcceleration() = 0;
-
-  /**
-   * Common interface for getting the maximum angular acceleration.
-   * 
-   * @return The maximum angular acceleration in radians per second squared.
-   */
-  virtual units::radians_per_second_squared_t getMaxAngularAcceleration() = 0;
-
-  /**
-   * Common interface for setting the coversion factor from linear to angular units.
-   * 
-   * @param conversionFactor The conversion factor from linear to angular units (such as a wheel
-   *                         diameter or gear ratio)
-   */
-  virtual void setConversion(units::compound_unit<units::radians, units::inverse<units::meters>> conversionFactor) = 0;
-
-  /**
-   * Common interface for getting the coversion factor from linear to angular units.
-   * 
-   * @return The conversion factor from linear to angular units (such as a wheel diameter or gear ratio)
-   */
-  virtual units::compound_unit<units::radians, units::inverse<units::meters>> getConversion() = 0;
+  virtual units::meter_t angularToLinear(units::radian_t angle) const = 0;
 
     /**
-   * Common interface for inverting direction of a motor.
+   * Common interface for inverting direction of a mechanism.
    *
    * @param isInverted The state of inversion, true is inverted.
    */
   virtual void setInverted(bool isInverted) = 0;
 
     /**
-   * Common interface for returning the inversion state of a motor.
+   * Common interface for returning the inversion state of a mechanism.
    *
    * @return isInverted The state of inversion, true is inverted.
    */
   virtual bool getInverted() const = 0;
 
   /**
-   * Common interface for disabling a motor.
+   * Common interface for disabling a mechanism.
    */
   virtual void disable() = 0;
 
   /**
-   * Common interface to stop the motor until `SetPosition` is called again.
+   * Common interface to stop the mechanism until `setPosition` is called again.
    */
   virtual void stopMotor() = 0;
 };
--- include/rmb/motorcontrol/VelocityController.h
+++ include/rmb/motorcontrol/VelocityController.h
@@ -1,35 +1,38 @@
 
 #pragma once
 
+#include <functional>
+#include <assert.h>
+
 #include <units/velocity.h>
 #include <units/acceleration.h>
 #include <units/angular_velocity.h>
 #include <units/angular_acceleration.h>
 
-#include <frc/motorcontrol/MotorController.h>
-
 namespace rmb {
 
 /**
- * Interface for controlling motor velocity used by wrappers of device specific 
- * APIs. The interface is meant to descibe what a motor should do, but not how 
- * should do it. Methods are given in both linear and angular units but controll 
- * the same motor, so changes to the target linear velocity also modify the 
- * target angular velocity and vice versa.
+ * Interface for controlling a mechanisms velocity used by wrappers of device 
+ * specific APIs. The interface is meant to descibe what a mechanism should do,
+ * not how should do it. Methods are given in both linear and angular units but 
+ * controll the same mechanism, so changes to the target linear velocity also 
+ * modify the target angular velocity and vice versa. Linear methods have been 
+ * given default implintations that rely on angular methods under the assumtion 
+ * that the mechanism is powered by a motor. If this is not the case, 
+ * re-implement or mark methods unimplemented as nessesary.
  * 
  * @see SparkMaxVelocityController TalonSRXVelocityController 
- *      FalconVelocityController
+ *      FalconVelocityController FeedforwardVelocityController
  */
 class VelocityController {
 public:
 
   /**
    * Common interface for setting the target linear velocity.
-   * positon
    * 
    * @param velocity The target linear velocity in meters per second.
    */
-  virtual void setVelocity(units::meters_per_second_t velocity) = 0;
+  virtual void setVelocity(units::meters_per_second_t velocity);
 
   /**
    * Common interface for setting the target angular velocity.
@@ -40,147 +43,125 @@ public:
 
   /**
    * Common interface for getting the <b>current</b> linear velocity of the
-   * motor regardless of target.
+   * mechanism regardless of target.
    * 
    * @return The <b>current</b> linear velocity in meters per second.
    */
-  virtual units::meters_per_second_t getVelocity() = 0;
+  virtual units::meters_per_second_t getVelocity() const;
 
   /**
    * Common interface for getting the <b>current</b> angular velocity of the
-   * motor regardless of target.
+   * mechanism regardless of target.
    * 
    * @return The <b>current</b> angular velocity in radians per second
    */
-  virtual units::radians_per_second_t getAngularVelocity() = 0;
+  virtual units::radians_per_second_t getAngularVelocity() const = 0;
 
   /**
    * Common interface for getting the <b>target</b> linear velocity.
    * 
    * @return The <b>target</b> linear velocity in meters per second.
    */
-  virtual units::meters_per_second_t getTargetVelocity() = 0;
+  virtual units::meters_per_second_t getTargetVelocity() const;
 
   /**
    * Common interface for getting the <b>target</b> angular velocity.
    * 
    * @return The <b>target</b> angular velocity in radians per second.
    */
-  virtual units::radians_per_second_t getTargetAngularVelocity() = 0;
+  virtual units::radians_per_second_t getTargetAngularVelocity() const = 0;
 
   /**
    * Common interface for getting the <b>current</b> linear velocity error.
    * 
-   * @return The diffrence between the actual linear velocity and target linear velocity.
+   * @return The diffrence between the actual and target linear velocitys.
    */
-  virtual units::meters_per_second_t getError() = 0;
+  virtual units::meters_per_second_t getError() const;
 
   /**
    * Common interface for getting the <b>current</b> angular velocity error.
    * 
-   * @return The diffrence between the actual angular velocity and target angular velocity.
+   * @return The diffrence between the actual and target angular velocitys.
    */
-  virtual units::radians_per_second_t getAngularError() = 0;
+  virtual units::radians_per_second_t getAngularError() const;
 
   /**
-   * Common interface for returning whetther the motor is at the target velocity.
+   * Common interface for returning whether the mechanism is at the target 
+   * velocity.
    * 
    * @return true if it at the target, and false if it is not.
    */
-  virtual bool atTarget() = 0;
+  virtual bool atTarget() const = 0;
 
   /**
    * Common interface for setting the maximum desired linear velocity.
    * 
-   * @param maxVelocity The maximum linear velocity of a motor.
+   * @param maxVelocity The maximum linear velocity of a mechanism.
    */
-  virtual void setMaxVelocity(units::radians_per_second_t maxVelocity) = 0;
+  virtual void setMaxVelocity(units::radians_per_second_t maxVelocity);
 
   /**
    * Common interface for setting the maximum desired angular velocity.
    * 
-   * @param maxVelocity The maximum angular velocity of a motor.
+   * @param maxVelocity The maximum angular velocity of a mechanism.
    */
   virtual void setMaxAngularVelocity(units::radians_per_second_t maxVelocity) = 0;
 
   /**
    * Common interface for getting the maximum desired linear velocity.
    * 
-   * @return The maximum linear velocity of a motor.
+   * @return The maximum linear velocity of a mechanism.
    */
-  virtual units::radians_per_second_t getMaxVelocity() = 0;
+  virtual units::radians_per_second_t getMaxVelocity() const;
 
   /**
    * Common interface for getting the maximum desired angular velocity.
    * 
-   * @return The maximum angular velocity of a motor.
+   * @return The maximum angular velocity of a mechanism.
    */
-  virtual units::radians_per_second_t getMaxAngularVelocity() = 0;
+  virtual units::radians_per_second_t getMaxAngularVelocity() const = 0;
 
   /**
-   * Common interface for setting the maximum desired linear accleration.
+   * Common interface for converting from linear to angular units. A functions
+   * is used rather than a conversion factor since some conversions may be
+   * non-linear. 
    * 
-   * @param maxAcceleration The maximum linear acceleration of a motor.
+   * @param linear The linear distance in meters.
+   * @return The angular distance in radians.
    */
-  virtual void setMaxAcceleration(units::meters_per_second_squared_t maxAcceleration) = 0;
+  virtual units::radians_per_second_t linearToAngular(units::meters_per_second_t linear) const = 0;
 
   /**
-   * Common interface for setting the maximum desired angular accleration.
+   * Common interface for converting from angular to linear units. A functions
+   * is used rather than a conversion factor since some conversions may be
+   * non-linear. 
    * 
-   * @param maxAcceleration The maximum angular acceleration of a motor.
+   * @param angle The linear distance in radians.
+   * @return The linear distance in meters.
    */
-  virtual void setMaxAngularAcceleration(units::radians_per_second_squared_t maxAcceleration) = 0;
+  virtual units::meters_per_second_t angularToLinear(units::radians_per_second_t angle) const  = 0;
 
   /**
-   * Common interface for getting the maximum desired linear accleration.
-   * 
-   * @return The maximum linear acceleration of a motor.
-   */
-  virtual units::meters_per_second_squared_t getMaxAcceleration() = 0;
-
-  /**
-   * Common interface for getting the maximum desired angular accleration.
-   * 
-   * @return The maximum angular acceleration of a motor.
-   */
-  virtual units::radians_per_second_squared_t getMaxAngularAcceleration() = 0;
-
-  /**
-   * Common interface for setting the coversion factor from linear to angular units.
-   * 
-   * @param conversionFactor The conversion factor from linear to angular units (such as a wheel
-   *                         diameter or gear ratio)
-   */
-  virtual void setConversion(units::compound_unit<units::radians, units::inverse<units::meters>> conversionFactor) = 0;
-
-  /**
-   * Common interface for getting the coversion factor from linear to angular units.
-   * 
-   * @return The conversion factor from linear to angular units (such as a wheel diameter or gear ratio)
-   */
-  virtual units::compound_unit<units::radians, units::inverse<units::meters>> getConversion() = 0;
-
-  /**
-   * Common interface for inverting direction of a motor.
+   * Common interface for inverting direction of a mechanism.
    *
    * @param isInverted The state of inversion, true is inverted.
    */
   virtual void setInverted(bool isInverted) = 0;
 
   /**
-   * Common interface for returning the inversion state of a motor.
+   * Common interface for returning the inversion state of a mechanism.
    *
    * @return isInverted The state of inversion, true is inverted.
    */
   virtual bool getInverted() const = 0;
 
   /**
-   * Common interface for disabling a motor.
+   * Common interface for disabling a mechanism.
    */
   virtual void disable() = 0;
 
   /**
-   * Common interface to stop the motor until `setVelocity` is called again.
+   * Common interface to stop the mechanism until `setVelocity` is called again.
    */
   virtual void stopMotor() = 0;
 };
