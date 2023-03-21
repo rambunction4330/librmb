e0f3f59fc461c9201aa8222c0de20c49943a7eb9
gcjurgiel <gcjurgiel@icloud.com>
2023-03-21 09:00:08 -0400

    Combine odometry and drive

--- src/rmb/drive/DifferentialDrive.cpp
+++ src/rmb/drive/DifferentialDrive.cpp
@@ -2,13 +2,90 @@
 
 #include <frc/drive/DifferentialDrive.h>
 
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableInstance.h>
+#include <networktables/DoubleArrayTopic.h>
+
 namespace rmb {
 
 DifferentialDrive::DifferentialDrive(
-    std::shared_ptr<LinearVelocityController> l,
-    std::shared_ptr<LinearVelocityController> r,
-    frc::DifferentialDriveKinematics k)
-    : left(l), right(r), kinematics(k) {}
+  std::unique_ptr<LinearVelocityController> left, 
+  std::unique_ptr<LinearVelocityController> right,
+  const frc::Gyro& gyro, frc::DifferentialDriveKinematics kinematics,
+  frc::RamseteController ramseteController, std::string visionTable,
+  const frc::Pose2d &initalPose) : 
+  left(std::move(left)), right(std::move(right)), gyro(gyro), kinematics(kinematics), 
+  ramseteController(ramseteController), 
+  poseEstimator(kinematics, gyro.GetRotation2d(), left->getPosition(), 
+                right->getPosition(), initalPose){
+
+ // Get vision table.
+  nt::NetworkTableInstance inst = nt::NetworkTableInstance::GetDefault();
+  auto table = inst.GetTable(visionTable);
+
+  // Get positon and standard deviation topics.
+  poseSubscriber = table->GetDoubleArrayTopic("pose").Subscribe({});
+  stdDevSubscriber = table->GetDoubleArrayTopic("stdDev").Subscribe({});
+
+  // Create positon callback
+  poseListener = inst.AddListener(
+      poseSubscriber, nt::EventFlags::kValueAll,
+      [this](const nt::Event &event) {
+        // Get timestamped data.
+        nt::TimestampedDoubleArray rawData = poseSubscriber.GetAtomic();
+
+        // Check data format.
+        if (rawData.value.size() != 3) {
+          return;
+        }
+
+        // Extract positon and time.
+        frc::Pose2d pose = {units::meter_t(rawData.value[0]),
+                            units::meter_t(rawData.value[1]),
+                            units::radian_t(rawData.value[2])};
+
+        units::second_t time = units::microsecond_t(rawData.time);
+
+        // Lock mutex for thread saftey and add vision.
+        std::lock_guard<std::mutex> lock(visionThreadMutex);
+        poseEstimator.AddVisionMeasurement(pose, time);
+      });
+
+  // Create standard deviation callback
+  stdDevListener =
+      inst.AddListener(stdDevSubscriber, nt::EventFlags::kValueAll,
+                       [this](const nt::Event &event) {
+                         // Get data from table.
+                         std::vector<double> rawData = stdDevSubscriber.Get();
+
+                         // Check data format
+                         if (rawData.size() != 3) {
+                           return;
+                         }
+
+                         // Lock mutex for thread saftey and add vision.
+                         std::lock_guard<std::mutex> lock(visionThreadMutex);
+                         poseEstimator.SetVisionMeasurementStdDevs(
+                             {rawData[0], rawData[1], rawData[2]});
+                       });
+}
+
+DifferentialDrive::DifferentialDrive(
+  std::unique_ptr<LinearVelocityController> left, 
+  std::unique_ptr<LinearVelocityController> right,
+  const frc::Gyro& gyro, frc::DifferentialDriveKinematics kinematics,
+  frc::RamseteController ramseteController,
+  const frc::Pose2d &initalPose) : 
+  left(std::move(left)), right(std::move(right)), gyro(gyro), 
+  kinematics(kinematics), ramseteController(ramseteController), 
+  poseEstimator(kinematics, gyro.GetRotation2d(), left->getPosition(), 
+                right->getPosition(), initalPose){}
+
+DifferentialDrive::~DifferentialDrive() {
+  // Remove listeners.
+  nt::RemoveListener(poseListener);
+  nt::RemoveListener(stdDevListener);
+}
 
 void DifferentialDrive::arcadeDrive(double xSpeed, double zRotation) {
   auto wheelSpeeds =
@@ -44,8 +121,36 @@ void DifferentialDrive::driveWheelSpeeds(
   right->setVelocity(wheelSpeeds.right);
 }
 
+frc::DifferentialDriveWheelSpeeds DifferentialDrive::getWheelSpeeds() const {
+  return {left->getVelocity(), right->getVelocity()};
+}
+
 void DifferentialDrive::driveChassisSpeeds(frc::ChassisSpeeds chassisSpeeds) {
   driveWheelSpeeds(kinematics.ToWheelSpeeds(chassisSpeeds));
+}
+
+frc::ChassisSpeeds DifferentialDrive::getChassisSpeeds() const {
+  return kinematics.ToChassisSpeeds(getWheelSpeeds());
+}
+
+frc::Pose2d DifferentialDrive::getPose() const {
+  // Lock thread for saftey and get positon.
+  std::lock_guard<std::mutex> lock(visionThreadMutex);
+  return poseEstimator.GetEstimatedPosition();
+}
+
+frc::Pose2d DifferentialDrive::updatePose() {
+  // Lock thread for saftey and updte position
+  std::lock_guard<std::mutex> lock(visionThreadMutex);
+  return poseEstimator.Update(gyro.GetRotation2d(), left->getPosition(),
+                              right->getPosition());
+}
+
+void DifferentialDrive::resetPose(const frc::Pose2d &pose) {
+  // Lock thread for saftey and reset position
+  std::lock_guard<std::mutex> lock(visionThreadMutex);
+  poseEstimator.ResetPosition(gyro.GetRotation2d(), left->getPosition(),
+                              right->getPosition(), pose);
 }
 
 } // namespace rmb
--- src/rmb/drive/DifferentialDrive.h
+++ src/rmb/drive/DifferentialDrive.h
@@ -2,21 +2,50 @@
 #pragma once
 
 #include <memory>
+#include <string>
+#include <mutex>
 
 #include <units/velocity.h>
 
-#include <frc/kinematics/ChassisSpeeds.h>
 #include <frc/kinematics/DifferentialDriveKinematics.h>
 #include <frc/kinematics/DifferentialDriveWheelSpeeds.h>
+#include <frc/kinematics/ChassisSpeeds.h>
+#include <frc/interfaces/Gyro.h>
+#include <frc/controller/RamseteController.h>
+#include <frc/estimator/DifferentialDrivePoseEstimator.h>
+
+#include <networktables/DoubleArrayTopic.h>
+#include <networktables/NetworkTable.h>
 
 #include "rmb/motorcontrol/LinearVelocityController.h"
 
 namespace rmb {
 class DifferentialDrive {
 public:
-  DifferentialDrive(std::shared_ptr<LinearVelocityController> left,
-                    std::shared_ptr<LinearVelocityController> right,
-                    frc::DifferentialDriveKinematics kinematics);
+
+  DifferentialDrive(const DifferentialDrive&) = delete;
+  DifferentialDrive(DifferentialDrive&&) = default;
+
+  DifferentialDrive(std::unique_ptr<LinearVelocityController> left,
+                    std::unique_ptr<LinearVelocityController> right,
+                    const frc::Gyro& gyro,
+                    frc::DifferentialDriveKinematics kinematics,
+                    frc::RamseteController ramseteController,
+                    std::string visionTable,
+                    const frc::Pose2d &initalPose = frc::Pose2d());
+
+  DifferentialDrive(std::unique_ptr<LinearVelocityController> left,
+                    std::unique_ptr<LinearVelocityController> right,
+                    const frc::Gyro& gyro,
+                    frc::DifferentialDriveKinematics kinematics,
+                    frc::RamseteController ramseteController,
+                    const frc::Pose2d &initalPose = frc::Pose2d());
+
+  ~DifferentialDrive();
+
+  //---------------
+  // Drive Methods
+  //---------------
 
   void arcadeDrive(double xSpeed, double zRotation);
   void curvatureDrive(double xSpeed, double zRotation, bool turnInPlace);
@@ -25,10 +54,100 @@ public:
   void driveWheelSpeeds(units::meters_per_second_t leftVelocity,
                         units::meters_per_second_t rightVelocity);
   void driveWheelSpeeds(frc::DifferentialDriveWheelSpeeds wheelSpeeds);
+  frc::DifferentialDriveWheelSpeeds getWheelSpeeds() const;
+
   void driveChassisSpeeds(frc::ChassisSpeeds chassisSpeeds);
+  frc::ChassisSpeeds getChassisSpeeds() const;
+
+  //------------------
+  // Odometry Methods
+  //------------------
+
+  /**
+   * Returns the current poition without modifying it.
+   */
+  frc::Pose2d getPose() const;
+
+  /**
+   * Updates the current position of the robot using encoder and gyroscope
+   * data. 
+   * 
+   * *Note:* Vision estimations are updated on a separate thread generated at
+   * object construction.
+   *
+   * @return The updated position.
+   */
+  frc::Pose2d updatePose();
+
+  /**
+   * Resets the estimated robot poition.
+   *
+   * @return The position to reset the robots estimated position to.
+   */
+  void resetPose(const frc::Pose2d &pose = frc::Pose2d());
+
+  //----------------------
+  // Trajectory Following
+  //----------------------
+
+  // TODO: Add Trajectoyr Following Methods
+
+  // frc2::CommandPtr followWPILibTrajectory();
+  // frc2::ComnandPtr followPPTrajectory();
+  // frc2::CommandPtr followPPTrajectoryGroup();
+  // frc2::CommandPtr followPPTrajectoryWithEvents();
+  // frc2::CommandPtr followPPFullAuto();
 
 private:
-  std::shared_ptr<LinearVelocityController> left, right;
+
+  //-----------------
+  // Drive Variables
+  //-----------------
+
+  std::unique_ptr<LinearVelocityController> left, right;
+  const frc::Gyro& gyro;
   frc::DifferentialDriveKinematics kinematics;
+  frc::RamseteController ramseteController;
+
+  //-------------------
+  // Odometry Variables
+  //-------------------
+
+  /** Object to handle the math behind pose estimation. */
+  frc::DifferentialDrivePoseEstimator poseEstimator;
+
+  //---------------
+  // Vision Thread
+  //---------------
+
+  /**
+   * Network Tables subscriber for vision positon predictions.
+   *
+   * Datya is sent in a three entry double array ordered X, Y, Theta. X and Y
+   * are in meters and Theta is in radians.
+   */
+  nt::DoubleArraySubscriber poseSubscriber;
+
+  /**
+   * Network Tables subscriber for the standard deviation of vision predictions.
+   *
+   * Data is sent in a three entry double array ordered X, Y, Theta. X and Y
+   * are in meters and Theta is in radians. The standard deviations are a
+   * measure of how much the reported positon typically deviates from the true
+   * positon. Larger values denote less accuracy in vision data, while smaller
+   * values denote higher accuracy.
+   */
+  nt::DoubleArraySubscriber stdDevSubscriber;
+
+  /** Handle for keeping track of vision posion callback for position. */
+  NT_Listener poseListener;
+
+  /** Handle for keeping track of vision positon standard deviations callback
+   * for position.
+   */
+  NT_Listener stdDevListener;
+
+  /** mutex to protect position estimations between vision threads. */
+  mutable std::mutex visionThreadMutex;
 };
 } // namespace rmb
